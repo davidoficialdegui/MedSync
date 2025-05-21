6206bcbc01bd62e6280d2ecdf6f616909a3043c6
David Oficialdegui <david.oficialdegui@opendeusto.es>
2025-05-21 18:05:02 +0200

    autenticacion .c y .h modif

---
+++ src/autenticacion.h
@@ -0,0 +1,10 @@
+#ifndef AUTENTICACION_H
+#define AUTENTICACION_H
+
+#include <sqlite3.h>
+
+typedef enum { ROLE_UNKNOWN=0, ROLE_ADMIN, ROLE_MEDICO, ROLE_PACIENTE } Role;
+
+Role autenticar(sqlite3 *db, const char *username, const char *password);
+
+#endif 
