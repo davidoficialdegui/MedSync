44a4f62c949d3b6d81da5cae9345ddb9a7fab48d
Jorge Cobián Vidal <j.cobian@opendeusto.es>
2025-05-21 18:13:31 +0200

    correccion
    corregimos

--- src/paciente.h
+++ src/paciente.h
@@ -6,7 +6,7 @@
 int paciente_create(sqlite3 *db,  const char *nombre, const char *apellidos, const char *fecha_nac, const char *genero,  const char *telefono, const char *email);
 
 int paciente_list(sqlite3 *db);
-int paciente_update(sqlite3 *db,  int id,  const char *nombre,  const char *apellidos  const char *fecha_nac,   const char *genero,  const char *telefono, const char *email);
+int paciente_update(sqlite3 *db,  int id,  const char *nombre,  const char *apellidos , const char *fecha_nac,   const char *genero,  const char *telefono, const char *email);
 int paciente_delete(sqlite3 *db, int id);
 
 #endif // PACIENTE_H
