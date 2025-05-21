4a8b4bca00da3f1d26728006cb4c57997953726f
David Oficialdegui <david.oficialdegui@opendeusto.es>
2025-05-21 18:03:29 +0200

    admin h

--- src/admin.h
+++ src/admin.h
@@ -1,37 +1,12 @@
-#include "sqlite3.h"
 #ifndef ADMIN_H
 #define ADMIN_H
 
-typedef struct
-{
-    int id;
-    char nombre[50];
-    char apellido1[50];
-    char apellido2[50];
-    char dni[10];
-    char cargo[50];
-    char telefono[15];
-    char email[100];
-} Empleado;
+#include <sqlite3.h>
 
-// metodos para paciente
-void gestionarPacientes(sqlite3 *db);
-void registrarNuevoPaciente(sqlite3 *db);
-void BuscarPaciente(sqlite3 *db);
-void EditarPaciente(sqlite3 *db);
-void EliminaPaciente(sqlite3 *db);
+int admin_create(sqlite3 *db, const char *username, const char *password);
 
-// metodos para empleado
-void gestionarEmpleados(sqlite3 *db);
-//void registrarNuevoEmpleado(sqlite3 *db);
-void BuscarEmpleado(sqlite3 *db);
-void EditarEmpleado(sqlite3 *db);
-void EliminaEmpleado(sqlite3 *db);
+int admin_list(sqlite3 *db);
 
-// metodos para reporte
-void generarReportes(sqlite3 *db);
-void reportePacientes(sqlite3 *db);
-void reporteEmpleados(sqlite3 *db);
-void reporteMedicos(sqlite3 *db);
+int admin_authenticate(sqlite3 *db, const char *username, const char *password);
 
-#endif
+#endif 
